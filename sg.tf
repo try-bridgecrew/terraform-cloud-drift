0d649aee373997d583e4521ad1e95154cf9294c0
Barak Schoster Goihman <schosterbarak@gmail.com>
2021-01-28 16:27:04 +0200

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,13 +1,14 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     description = "TLS from VPC"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   egress {
@@ -20,4 +21,9 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = "allow_tls"
   }
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
